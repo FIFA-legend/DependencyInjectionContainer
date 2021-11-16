ea2c1c895c13a60ce008a18765007b87df42f4eb
FIFA-legend <58667610+FIFA-legend@users.noreply.github.com>
2021-11-16 22:15:59 +0300

    Add files via upload

---
+++ DIContainer/ImplementationInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DIContainer
+{
+    public class ImplementationInfo
+    {
+        public readonly Type ImplClassType;
+
+        public readonly LifeTime LifeTime;
+
+        public ImplementationInfo(LifeTime lt, Type impl)
+        {
+            ImplClassType = impl;
+            LifeTime = lt;
+        }
+    }
+}
---
+++ Tests/ContainerTests.cs
@@ -0,0 +1,133 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DIContainer;
+using System.Collections.Generic;
+
+namespace Tests
+{
+    [TestClass]
+    public class ContainerTests
+    {
+        [TestMethod]
+        public void SimpleDependencyTest()
+        {
+            DependenciesConfiguration configuration = new DependenciesConfiguration();
+            configuration.Register<ISmth, ISmthImpl>(LifeTime.Singleton);
+            DependenciesProvider provider = new DependenciesProvider(configuration);
+
+            ISmthImpl cl = (ISmthImpl) provider.Resolve<ISmth>();
+            Assert.IsNotNull(cl);
+        }
+
+        [TestMethod]
+        public void DifferentLifeTimeTest()
+        {
+            DependenciesConfiguration configuration = new DependenciesConfiguration();
+            configuration.Register<ISmth, ISmthImpl>(LifeTime.Singleton);
+            configuration.Register<IService, FirstIServiceImpl>();
+            DependenciesProvider provider = new DependenciesProvider(configuration);
+
+            ISmthImpl cl1 = (ISmthImpl) provider.Resolve<ISmth>();
+            ISmthImpl cl2 = (ISmthImpl) provider.Resolve<ISmth>();
+            Assert.AreEqual(cl1, cl2);
+            IService s1 = provider.Resolve<IService>();
+            IService s2 = provider.Resolve<IService>();
+            Assert.AreNotEqual(s1, s2);
+        }
+
+        [TestMethod]
+        public void ManyImplementationsTest()
+        {
+            DependenciesConfiguration configuration = new DependenciesConfiguration();
+            configuration.Register<IService, FirstIServiceImpl>();
+            configuration.Register<IService, SecondIServiceImpl>();
+            DependenciesProvider provider = new DependenciesProvider(configuration);
+
+            IEnumerable<IService> impls = provider.Resolve<IEnumerable<IService>>();
+            Assert.IsNotNull(impls);
+            Assert.AreEqual(2, ((List<IService>) impls).Count);
+        }
+
+        [TestMethod]
+        public void InnerDependencyTest()
+        {
+            DependenciesConfiguration configuration = new DependenciesConfiguration();
+            configuration.Register<ISmth, ISmthImpl>();
+            configuration.Register<IService, FirstIServiceImpl>();
+            configuration.Register<IService, SecondIServiceImpl>();
+            configuration.Register<IClient, SecondIClientImpl>();
+            DependenciesProvider provider = new DependenciesProvider(configuration);
+
+            FirstIServiceImpl cl1 = (FirstIServiceImpl) provider.Resolve<IService>();
+            Assert.IsNotNull(cl1.Smth);
+
+            SecondIClientImpl cl2 = (SecondIClientImpl) provider.Resolve<IClient>();
+            Assert.IsNotNull(cl2.Serv);
+            Assert.AreEqual(2, ((List<IService>) cl2.Serv).Count);
+        }
+
+        [TestMethod]
+        public void SimpleRecursionTest()
+        {
+            DependenciesConfiguration configuration = new DependenciesConfiguration();
+            configuration.Register<IClient, FirstIClientImpl>();
+            configuration.Register<IData, IDataImpl>();
+            DependenciesProvider provider = new DependenciesProvider(configuration);
+
+            FirstIClientImpl client = (FirstIClientImpl) provider.Resolve<IClient>();
+            Assert.IsNull(((IDataImpl) client.Data).Cl);
+        }
+
+        [TestMethod]
+        public void SimpleOpenGenericTest()
+        {
+            DependenciesConfiguration configuration = new DependenciesConfiguration();
+            configuration.Register<IAnother<ISmth>, First<ISmth>>();
+            configuration.Register(typeof(IFoo<>), typeof(Second<>));
+            DependenciesProvider provider = new DependenciesProvider(configuration);
+
+            IAnother<ISmth> cl1 = provider.Resolve<IAnother<ISmth>>();
+            Assert.IsNotNull(cl1);
+            
+            IFoo<IService> cl2 = provider.Resolve<IFoo<IService>>();
+            Assert.IsNotNull(cl2);
+        }
+
+        [TestMethod]
+        public void OneClassTest()
+        {
+            DependenciesConfiguration configuration = new DependenciesConfiguration();
+            configuration.Register<HumanImpl, HumanImpl>();
+            DependenciesProvider provider = new DependenciesProvider(configuration);
+            HumanImpl humanImpl = provider.Resolve<HumanImpl>();
+            Assert.IsNotNull(humanImpl);
+        }
+    }
+
+    interface IAnother<T>
+        where T : ISmth
+    {
+    }
+
+    class First<T> : IAnother<T>
+        where T : ISmth
+    {
+    }
+
+    interface IFoo<T>
+        where T : IService
+    {
+    }
+
+    class Second<T> : IFoo<T>
+        where T : IService
+    {
+    }
+
+    public interface IHuman
+    {
+    }
+
+    public class HumanImpl : IHuman
+    {
+    }
+}
